2c51078b5428be6100b48bcd8da3237e70229fd3
roy-key <roykeynan@gmail.com>
2018-07-15 02:39:19 +0300

    master-SortedUpdatedKeyValueExtractor OptimizedSortedUpdatedKeyValueExtractor logic, related objects , utils and tests.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # sorted-updated-kv-extractor
-The SortedUpdatedKeyValueExtractor will receive a list of segments, which each segment would have a list of sorted key-value pairs, and will print in alphabetically order all the key-value pairs from all the segments
+The extractor will receive a list of segments, which each segment would have a list of sorted key-value pairs, and will print in alphabetically order all the key-value pairs from all the segments
+
+In each segment a key can appear at most one time, but the same key could be in another segment.
+then the key-value pair should be taken from the most updated segment, i.e the segment which has the bigger index in the segments array.
+
+Currently, for simplicity and understating, all the segments data is loaded into memory.
+To support very big segments it could be loaded in chucks of data from files or db;
+
+ 
+#### SortedUpdatedKeyValueExtractor
+
+Each time the first M of key-value pairs will be loaded from all the N segments, and after it, another M
+key-value pairs would be loaded from that specific segment, until all its kv would be handled.
+
+the algorithm would halt after all the kv from all the segments had been handled.
+
+Complexity:
+ 
+ Where there are N segments and M is the biggest amount of kv from some segment I where 0<=I<=N.
+ Each insertion to the Segmented min heap is of O(log(M)).
+
+    Run time: O(N) * (M) * O(log(N)) = O(N*M*log(N)).
+    Memory: O(N) additional space.
+   
+    
+#### OptimizedSortedUpdatedKeyValueExtractor
+This package also supports an optimized OptimizedSortedUpdatedKeyValueExtractor, which implements the same logic as the SortedUpdatedKeyValueExtractor in a different way.
+
+    With additional max O(N) memory, it will visit all N*M key-values, but will insert each key only once, meaning that the O(log(M)) insertions will be as many times as the number of different keys.
+        
+ If the number of segments (N) is significantly large and its not likely that a key would be updated much, use the SortedUpdatedKeyValueExtractor, else, always prefer the OptimizedSortedUpdatedKeyValueExtractor 
+
+
+
+## How to use
+
+1. npm install
+2. Create an instance of either SortedUpdatedKeyValueExtractor, OptimizedSortedUpdatedKeyValueExtractor and print the segments.
+
+There are some examples in the tests which could assist
---
+++ test/optimizied-sorted-updated-key-value-extractor.test.js
@@ -0,0 +1,159 @@
+'use strict';
+
+const assert = require('assert');
+const given = require('mocha-testdata');
+const OptimizedSortedUpdatedKeyValueExtractor = require('../lib/extractor/optimized-sorted-updated-key-value-extractor');
+
+describe('Print alphabetically sorted key value pairs from the most updated segment ', () => {
+
+    given(
+        [
+            [[['a:15']], [
+                {
+                    "segmentId": 0,
+                    "element": "a:15"
+                }
+            ]],
+            [[['a:15'], ['a:20'], ['a:30'], ['a:10']], [
+                {
+                    "segmentId": 3,
+                    "element": "a:10"
+                }
+            ]],
+            [[['a:15'], ['b:15'], ['c:15'], ['d:15']], [
+                {
+                    "segmentId": 0,
+                    "element": "a:15"
+                },
+                {
+                    "segmentId": 1,
+                    "element": "b:15"
+                },
+                {
+                    "segmentId": 2,
+                    "element": "c:15"
+                },
+                {
+                    "segmentId": 3,
+                    "element": "d:15"
+                }
+            ]],
+            [[['d:15'], ['c:15'], ['b:15'], ['a:15']], [
+                {
+                    "segmentId": 3,
+                    "element": "a:15"
+                },
+                {
+                    "segmentId": 2,
+                    "element": "b:15"
+                },
+                {
+                    "segmentId": 1,
+                    "element": "c:15"
+                },
+                {
+                    "segmentId": 0,
+                    "element": "d:15"
+                }
+            ]],
+            [[['a:15'], ['b:15'], ['b:20'], ['a:20']], [
+                {
+                    "segmentId": 3,
+                    "element": "a:20"
+                },
+                {
+                    "segmentId": 2,
+                    "element": "b:20"
+                },
+            ]],
+            [[['ad:15'], ['ac:15'], ['bb:20'], ['ba:20']], [
+                {
+                    "segmentId": 1,
+                    "element": "ac:15"
+                },
+                {
+                    "segmentId": 0,
+                    "element": "ad:15"
+                },
+                {
+                    "segmentId": 3,
+                    "element": "ba:20"
+                },
+                {
+                    "segmentId": 2,
+                    "element": "bb:20"
+                }
+            ]],
+            [[['aaa:15', 'aab:15', 'aac:30', 'aad:20'], ['aab:15', 'abb:15', 'abc:30'], ['abc:15'], ['aab:15', 'aac:15', 'aad:30', 'ada:20']], [
+                {
+                    "segmentId": 0,
+                    "element": "aaa:15"
+                },
+                {
+                    "segmentId": 3,
+                    "element": "aab:15"
+                },
+                {
+                    "segmentId": 3,
+                    "element": "aac:15"
+                },
+                {
+                    "segmentId": 3,
+                    "element": "aad:30"
+                },
+                {
+                    "segmentId": 1,
+                    "element": "abb:15"
+                },
+                {
+                    "segmentId": 2,
+                    "element": "abc:15"
+                },
+                {
+                    "segmentId": 3,
+                    "element": "ada:20"
+                }
+            ]],
+            [[['a:0', 'b:0'], ['a:1', 'b:1', 'g:1', 'h:1'], ['a:2', 'f:2', 'g:2'], ['a:3', 'b:3', 'c:3', 'd:3'], ['b:4', 'c:4', 'd:4'], ['c:5', 'd:5'], ['d:6', 'e:6', 'f:6']], [
+                {
+                    "segmentId": 3,
+                    "element": "a:3"
+                },
+                {
+                    "segmentId": 4,
+                    "element": "b:4"
+                },
+                {
+                    "segmentId": 5,
+                    "element": "c:5"
+                },
+                {
+                    "segmentId": 6,
+                    "element": "d:6"
+                },
+                {
+                    "segmentId": 6,
+                    "element": "e:6"
+                },
+                {
+                    "segmentId": 6,
+                    "element": "f:6"
+                },
+                {
+                    "segmentId": 2,
+                    "element": "g:2"
+                },
+                {
+                    "segmentId": 1,
+                    "element": "h:1"
+                }
+            ]]
+        ]
+    ).test('Set: ', function (segments, expectedResults) {
+
+        const sortedUpdatedKeyValueExtractor = new OptimizedSortedUpdatedKeyValueExtractor(segments, true);
+        const result = sortedUpdatedKeyValueExtractor.print(segments);
+        assert.deepEqual(result, expectedResults);
+
+    });
+});
